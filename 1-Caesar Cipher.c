191bd9ceaf9d0eed8fab326f87fb445b49c6efd6
charan192210251 <129865777+charan192210251@users.noreply.github.com>
2024-09-17 14:03:47 +0530

    Update 1-Caesar Cipher.c

--- 1-Caesar Cipher.c
+++ 1-Caesar Cipher.c
@@ -1,22 +1,36 @@
-#include <stdio.h>
-int main() {
-    char text[100];
-    int k;
-    char c;
-    printf("Enter text: ");
-    scanf("%s", text);
-    char plain[100];
-    printf("Enter shift: ");
-    scanf("%d", &k);
-    for (int i = 0; text[i] != '\0'; i++) {
-        c = text[i];
-        plain[i]=text[i];
-        if (c >= 'A' && c <= 'Z')  // Uppercase letters
-            text[i] = (c - 'A' + k) % 26 + 'A';
-        else if (c >= 'a' && c <= 'z')  // Lowercase letters
-            text[i] = (c - 'a' + k) % 26 + 'a';
-    }
-    printf("Encrypted text: %s\n", text);
-    printf("Decrypted text after encryption: %s",plain);
-    return 0;
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char plain[100];
+	char cipher[100];
+	int k,i;
+	char c;
+	printf("Enter plain text: ");
+	scanf("%s",plain);
+	printf("Enter key: ");
+	scanf("%d",&k);
+	int l=strlen(plain);
+	//encryption
+	for(i=0;i<=l;i++){
+		c=plain[i];
+		if(c>='A'&&c<='Z'){
+			cipher[i]=(c-'A'+k)%26+'A';
+		}
+		else if(c>='a'&&c<='z'){
+			cipher[i]=(c-'a'+k)%26+'a';
+		}
+	}
+	printf("Enctrpted text: %s\n",cipher);
+	//decryption
+	for(i=0;i<=l;i++){
+		c=cipher[i];
+		if(c>='A'&&c<='Z'){
+			plain[i]=(c-'A'-k+26)%26+'A';
+		}
+		else if(c>='a'&&c<='z'){
+			plain[i]=(c-'a'-k+26)%26+'a';
+		}
+	}
+	printf("Decrypted text after encryption: %s",plain);
+	return 0;
 }
